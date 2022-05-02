41df4e320b2ec1512b1bcc97cec3e78b2ee6e457
theinfosecguy <keshavaarav22@gmail.com>
2022-05-02 19:00:51 +0530

    Add Security Headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,47 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "Expect-CT",
+    value: "enforce, max-age=86400",
+  },
+  {
+    key: "Content-Security-Policy",
+    value:
+      "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' data:; font-src 'self' data:;",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains; preload",
+  },
+];
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
