5968fda6919556e5177baa5fdb0af07f9e4a009c
Keshav Malik <keshavaarav22@gmail.com>
2022-05-20 17:06:50 +0530

    Added Unami Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
 import "../styles/fonts.css";
 import SEO from "../components/Seo";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="b7aaf9b4-fa87-48b9-be75-78d18aa0a35d"
+          src="https://analytics.vulnerable.live/umami.js"
+          data-domains="www.bugblogs.tech"
+        ></script>
+      </Head>
       <SEO />
       <Component {...pageProps} />
     </>
